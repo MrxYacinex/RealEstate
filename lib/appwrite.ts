a22208e5129aee01d3e11831dd88adcfa579aa19
Yacine Tadjine <yacine.tadjine@campus.tu-berlin.de>
2025-02-24 19:16:58 +0100

    Implement Appwrite client and OAuth2 login functionality for Google

---
+++ lib/appwrite.ts
@@ -0,0 +1,46 @@
+import{
+    Account,
+    Avatars,
+    Client,
+    OAuthProvider
+} from "react-native-appwrite";
+import * as Linking from 'expo-linking';
+
+export const config = {
+    platform: 'com.jsm.Real Estate',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(config.endpoint!)
+    .setEndpoint(config.projectId!)
+    .setPlatform(config.platform!)
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+    try {
+        const redirectUri = Linking.createURL('/');
+
+        const response = await account.createOAuth2Token(
+            OAuthProvider.Google, 
+            redirectUri
+        );
+
+        if(!response) throw new Error("Failed to Login");
+
+        const browserResult = await openAuthSessionAsync(
+            response.toString(),
+            redirectUri
+        )
+        
+        if(browserResult.type != 'success') throw new Error("Failed to Login");
+    } catch(error) {
+        console.error(error);
+        return false;
+    }
+}
